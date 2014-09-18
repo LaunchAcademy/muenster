4500c0578a63b32c5234ecaa87e135ed8823eee6
Adam Sheehan <a.t.sheehan@gmail.com>
2014-09-18 19:40:45 -0400

    Step 5: View a list of all companies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :jobs, only: [:index, :show]
 
+  resources :companies, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
