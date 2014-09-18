4f79e740d2a69a8e2b604eb4a8b522a7a5f7f766
Adam Sheehan <a.t.sheehan@gmail.com>
2014-09-18 19:55:54 -0400

    Step 8: Edit companies.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -22,6 +22,21 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+
+    if @company.update(company_params)
+      flash[:notice] = "Updated company."
+      redirect_to company_path(@company)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def company_params
