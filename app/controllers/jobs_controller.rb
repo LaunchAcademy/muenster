a8ed003dfa9e5ba66550216acd3747269a196f5b
Adam Sheehan <a.t.sheehan@gmail.com>
2014-09-18 20:01:45 -0400

    Step 9: Add a new job.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,27 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+    @job = Job.new
+    @companies = Company.all
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      flash[:notice] = "Added job."
+      redirect_to job_path(@job)
+    else
+      @companies = Company.all
+      render :new
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :company_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :jobs, only: [:index, :show]
+  resources :jobs, only: [:index, :show, :new, :create]
 
   resources :companies, only: [:index, :show, :new, :create, :edit, :update]
 
